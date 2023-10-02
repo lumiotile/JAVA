2a36a9ce5d33c3ac1a6de5500f3f63f74206e39d
María <130462647+soodita@users.noreply.github.com>
2023-10-02 09:46:02 +0200

    Solo javascript muestra fotos, falta css.

--- NASA/js/service.js
+++ NASA/js/service.js
@@ -1,5 +1,6 @@
 const apiKey = 'olt9u3kQQQb2zjNotJdqyuPUxi1gdZYC7KlmwUi3';
 const sol = 1000; // El número de sol que deseas consultar
+var cont = 0;
 
 // URL del API de la NASA
 const apiUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${sol}&api_key=${apiKey}`;
@@ -16,6 +17,17 @@ fetch(apiUrl)
   .then((data) => {
     // Los datos de las fotos se encuentran en el objeto 'data'
     console.log(data);
+    data.photos.forEach(element => {
+      var newDiv = document.createElement('div');
+      var newImg = document.createElement('img');
+      newDiv.className = 'divImagenes';
+      newImg.className = 'imagenes'
+      document.getElementById('container').appendChild(newDiv);
+      newImg.src = data.photos[cont].img_src;
+      newDiv.appendChild(newImg);
+      cont++;
+
+    });
     //data[0].img_src
     //document.getElementById("imagen").src = data.photos[0].img_src;
     data.photos[0].img_src
