f98e679c40b1c8e5c9ff12f3ec1481befc590ce0
María <130462647+soodita@users.noreply.github.com>
2023-10-25 10:01:40 +0200

    [25/10/2023]

--- PresenterYModel/src/Main.java
+++ PresenterYModel/src/Main.java
@@ -1,6 +1,28 @@
+import Interface.MyInterface;
+import Interface.Presenter;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+        MyInterface miPresentador = new Presenter();
+        Thread thread = new Thread(){
+          Scanner input = new Scanner(System.in);
+          int inputValue = 0;
+          while(true){
+                System.out.println("Simula un 0 - correcto");
+                System.out.println("Simula un 1 - incorrecto");
+                System.out.println("Dame un valor");
+                if (inputValue == 0) {
+                    System.out.println("Success");
+                    miPresentador.mostrarLoginCorrecto();
+                }else {
+                    System.out.println("Failure");
+                    miPresentador.mostararLoginIncorrecto();
+                }
+            }
+        };
     }
 }
